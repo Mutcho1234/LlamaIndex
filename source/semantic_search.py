7e1fdcc496f4ddd0c3cfff9d1063d10d6d415d1c
Mutcho1234 <mutcho1234abcd@gmail.com>
2025-01-05 20:58:02 +0900

    Add semantic search script using Llama Index

---
+++ source/semantic_search.py
@@ -0,0 +1,25 @@
+"""
+This script demonstrates how to use the Llama Index to perform semantic search on a directory of documents.
+"""
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+# Define constants
+REPO_PATH = r"C:\Users\mutch\source\repos\python-control"
+
+# Load the documents from the repository
+documents = SimpleDirectoryReader(REPO_PATH).load_data()
+
+# Create an index from the documents
+index = VectorStoreIndex.from_documents(documents)
+
+# Create a query engine from the index
+query_engine = index.as_query_engine()
+
+# Get a message from the user
+message = input("Enter a message: ")
+
+# Query the engine with the message
+response = query_engine.query(message)
+
+# Print the response
+print(response)
